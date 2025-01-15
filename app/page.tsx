adf1fbd78f4c1823ac629bb46d449ecf6d2b5839
unknown <katibepatience@gmail.com>
2025-01-15 15:47:00 +0300

    first

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@/components/ui/button'
-import { ArrowBigDownIcon, ArrowDown01Icon, ArrowRight, Save } from 'lucide-react'
+import { ArrowBigDownIcon, ArrowRight, Save } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
